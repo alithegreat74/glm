a8d7971fb6262914fa99aa6774ec875dabc3a5c4
Alireza Mohammadi <86660759+alithegreat74@users.noreply.github.com>
2024-09-21 23:36:24 +0330

    updated premake file

--- premake5.lua
+++ premake5.lua
@@ -2,16 +2,24 @@ project "glm"
     kind "StaticLib"
     language "C++"
     files{
+        "glm/**.cppm",
         "glm/**.hpp",
         "glm/**.inl",
+
         "glm/detail/**.hpp",
         "glm/detail/**.inl",
+        "glm/detail/**.cpp",
+
         "glm/ext/**.hpp",
         "glm/ext/**.inl",
+
         "glm/gtc/**.hpp",
         "glm/gtc/**.inl",
-        "glm/simd/**.hpp",
-        "glm/simd/**.inl"
+
+        "glm/gtx/**.hpp",
+        "glm/gtx/**.inl",
+
+        "glm/simd/**.h"
     }
-    includedirs "glm"
-    
+    includedirs ""
+    cppdialect "C++20"
